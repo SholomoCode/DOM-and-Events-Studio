37e535846c56b179bd9b3b40b5645428cd8b8dad
SholomoCode <worksolom5@gmail.com>
2022-09-23 15:19:46 -0500

    DOM-and-Events

--- scripts.js
+++ scripts.js
@@ -1,2 +1,25 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+window.addEventListener('load', function() {
+
+
+
+
+
+
+
+ let takeoffButton = this.document.getElementById("takeoff");
+ takeoffButton.addEventListener('click', function(){
+ response = window.confirm("Confirm that the shuttle is ready for takeoff.");
+ if(response){
+bg.style.backgroundColor = "blue";
+shuttleHeight.innerHTML = "10000";
+flightStatus.innerHTML = "Shuttle in flight";
+
+ }
+ });
+
+
+
+
+});
